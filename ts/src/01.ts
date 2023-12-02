6cf03f9b973734b5af00a1b3515689a21c932f95
Michael Fuchs <pubgrub@gmx.de>
2023-12-02 15:24:23 +0100

    added es-linting

--- ts/src/01.ts
+++ ts/src/01.ts
@@ -1,93 +1,95 @@
+import { readFileSync } from 'fs'
+
 /* Advent of Code 2023
 Day 01
 */
-let day = "01"
-let test: boolean  = false;
-let testPart1: boolean = false
-let testPart2: boolean  = true
 
-let testDataFile = '../testdata/' + day + ".txt";
-let dataFile = '../data/' + day + ".txt";
+const day = '01'
+const test: boolean = false
+const testPart1: boolean = false
+const testPart2: boolean = true
 
-import { readFileSync } from 'fs';
+const testDataFile = '../testdata/' + day + '.txt'
+const dataFile = '../data/' + day + '.txt'
 
-var inputLines = readFileSync( test ? testDataFile : dataFile).toString().split( "\n")
-inputLines = inputLines.map( str => ( str.trim()))
+let inputLines = readFileSync(test ? testDataFile : dataFile).toString().split('\n')
+inputLines = inputLines.map(str => (str.trim()))
 
 // Part 1
 
-const reFirstNumber = /(\d)/ 
+const reFirstNumber = /(\d)/
 const reLastNumber = /(\d)(?!.*\d)/
 
-var firstMatch, lastMatch;
-var sum = 0;
+let firstMatch, lastMatch
+let sum = 0
 
-// @ts-ignore
-if( test === false || testPart1 === true) {
-
-  for ( var i = 0; i < inputLines.length; i++) {
-    var s = inputLines[i]
-    if( s.length == 0) continue
-    firstMatch = s.match( reFirstNumber);
-    lastMatch = s.match( reLastNumber);
-    if ( firstMatch != null && lastMatch != null) {
+if (!test || testPart1) {
+  for (let i = 0; i < inputLines.length; i++) {
+    const s = inputLines[i]
+    if (s.length === 0) continue
+    firstMatch = s.match(reFirstNumber)
+    lastMatch = s.match(reLastNumber)
+    if (firstMatch != null && lastMatch != null) {
       sum += Number(firstMatch[1] + lastMatch[1])
     } else {
-      console.log( 'oops')
+      console.log('oops')
       process.exit(1)
     }
   }
 }
 
-console.log( "Result part 1: ", sum)
+console.log('Result part 1: ', sum)
 
 // Part 2
 
-const replacements: { [key: string]: string} = {
-  'one': '1', 'two': '2', 'three': '3', 'four': '4',
-  'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'}
+const replacements: Record<string, string> = {
+  one: '1',
+  two: '2',
+  three: '3',
+  four: '4',
+  five: '5',
+  six: '6',
+  seven: '7',
+  eight: '8',
+  nine: '9'
+}
 
-  const wordRegex = new RegExp( "^(" + Object.keys( replacements).join('|') + ")")
-  const digitRegex = new RegExp( "^(\\d)")
+const wordRegex = new RegExp('^(' + Object.keys(replacements).join('|') + ')')
+const digitRegex = /'^(\\d)'/
 
-
-// @ts-ignore
-if( test === false || testPart2 === true) {
-
-  sum = 0;
-  for ( var line = 0; line < inputLines.length; line++) {
-    var firstNum = '0'
-    var lastNum = '0'
-    var s = inputLines[line]
-    if( s.length == 0) continue
-    for( var start = 0; start < s.length; start++) {
-      var substr = s.substring( start)
-      var match = digitRegex.exec( substr)
-      if( match != null) {
-        if( firstNum == '0') {
+if (!test || testPart2) {
+  sum = 0
+  for (let line = 0; line < inputLines.length; line++) {
+    let firstNum = '0'
+    let lastNum = '0'
+    const s = inputLines[line]
+    if (s.length === 0) continue
+    for (let start = 0; start < s.length; start++) {
+      const substr = s.substring(start)
+      let match = digitRegex.exec(substr)
+      if (match != null) {
+        if (firstNum === '0') {
           firstNum = match[0]
         }
         lastNum = match[0]
         continue
       }
-      match = wordRegex.exec( substr)
-      if( match != null) {
-        if (firstNum == '0') {
+      match = wordRegex.exec(substr)
+      if (match != null) {
+        if (firstNum === '0') {
           firstNum = replacements[match[0]]
         }
         lastNum = replacements[match[0]]
         continue
-      }      
+      }
     }
-    if(firstNum != '0' && lastNum != '0') {
+    if (firstNum !== '0' && lastNum !== '0') {
       sum += Number(firstNum + lastNum)
     } else {
-      console.log( 'oops')
+      console.log('oops')
       process.exit(1)
     }
   }
 
-  console.log( "Result part 2: ", sum)
+  console.log('Result part 2: ', sum)
 }
-
-
--- ts/src/02.ts
+++ ts/src/02.ts
@@ -1,91 +1,92 @@
+import { readFileSync } from 'fs'
+
 /* Advent of Code 2023
 Day 01
 */
-let day = "02"
-let test: boolean  = false
-let testPart1: boolean = true
-let testPart2: boolean  = true
 
-let testDataFile = '../testdata/' + day + ".txt";
-let dataFile = '../data/' + day + ".txt";
+const day = '02'
+const test: boolean = false
 
-import { readFileSync } from 'fs';
+// const testPart1: boolean = true
+// const testPart2: boolean  = true
 
-var inputLines = readFileSync( test ? testDataFile : dataFile).toString().split( "\n")
-inputLines = inputLines.map( str => ( str.trim()))
+const testDataFile = '../testdata/' + day + '.txt'
+const dataFile = '../data/' + day + '.txt'
 
-var maxAllowed: { [key: string]: number} = 
-  { 'red': 12, 'green': 13, 'blue': 14}
+let inputLines = readFileSync(test ? testDataFile : dataFile).toString().split('\n')
+inputLines = inputLines.map(str => (str.trim()))
+
+const maxAllowed: Record<string, number> =
+  { red: 12, green: 13, blue: 14 }
 
 const gameRegex = /Game (\d+):\s(.*)/
-const takeRegex = /([a-z0-9 \,]+\;)/g
+const takeRegex = /([a-z0-9 ,]+;)/g
 const colorsRegex = /(\d+.*?[a-z]+)/g
 const colorNumRegex = /(\d+)\s([a-z]+)/
 
-var resultPart1 = 0
-var resultPart2 = 0
+let resultPart1 = 0
+let resultPart2 = 0
 
-for( var line of inputLines) {
-  if( line.length == 0)
+for (let line of inputLines) {
+  if (line.length === 0) {
     continue
-  var maxNum: { [key: string]: number} = {}
-  line = line + ";"
-  var gameMatch = line.match( gameRegex) 
-  if( gameMatch === null) {
-    console.log( "No match: ", line)
+  }
+  const maxNum: Record<string, number> = {}
+  line = line + ';'
+  const gameMatch = line.match(gameRegex)
+  if (gameMatch === null) {
+    console.log('No match: ', line)
     process.exit()
   }
-  var game = Number(gameMatch[1])
-  var takes = gameMatch[2].match( takeRegex)
-  if( takes === null) {
-    console.log( "No match: ", gameMatch[2])
+  const game = Number(gameMatch[1])
+  const takes = gameMatch[2].match(takeRegex)
+  if (takes === null) {
+    console.log('No match: ', gameMatch[2])
     process.exit()
   }
-  for( var take of takes) {
-    var colors = take.trim().match( colorsRegex)
-    if( colors === null) {
-      console.log( "No match: ", take)
+  for (const take of takes) {
+    const colors = take.trim().match(colorsRegex)
+    if (colors === null) {
+      console.log('No match: ', take)
       process.exit()
     }
-    for( var color of colors) {
-      var colorNum = color.match( colorNumRegex)
-      if( colorNum === null) {
-        console.log( "No match: ", color)
+    for (const color of colors) {
+      const colorNum = color.match(colorNumRegex)
+      if (colorNum === null) {
+        console.log('No match: ', color)
         process.exit()
       }
-      var num = Number(colorNum[ 1])
-      var colorStr = colorNum[2]
-      if( ! (colorStr in maxNum)) {
-        maxNum[ colorStr] = num
+      const num = Number(colorNum[1])
+      const colorStr = colorNum[2]
+      if (!(colorStr in maxNum)) {
+        maxNum[colorStr] = num
       } else {
-        if( maxNum[ colorStr] < num) {
-          maxNum[ colorStr] = num
+        if (maxNum[colorStr] < num) {
+          maxNum[colorStr] = num
         }
-      
       }
-
     }
   }
 
   // calc part 1
-  var valid = true
-  for( let [key, value] of Object.entries( maxAllowed)){
-    if( (key in maxNum) && (maxNum[key] > value)) {
+  let valid = true
+  for (const [key, value] of Object.entries(maxAllowed)) {
+    if ((key in maxNum) && (maxNum[key] > value)) {
       valid = false
       break
     }
   }
-  if( valid) {
+  if (valid) {
     resultPart1 += game
   }
 
   // calc part 2
-  var power = 1
-  for( let [ key, value] of Object.entries( maxNum)) {
+  let power = 1
+  for (const value of Object.values(maxNum)) {
     power *= value
   }
-  resultPart2 += power  
+  resultPart2 += power
 }
 
-console.log( "Result Part 1: ", resultPart1)
-console.log( "Result Part 2: ", resultPart2)
+console.log('Result Part 1: ', resultPart1)
+console.log('Result Part 2: ', resultPart2)
