cd85ccaf9a2a0f686d0e9012549fe0bbd6e2da5d
Michael Fuchs <pubgrub@gmx.de>
2023-12-06 22:29:30 +0100

    day 06 done

---
+++ ts/src/06.ts
@@ -0,0 +1,44 @@
+import { readFileSync } from 'fs'
+
+/* Advent of Code 2023
+Day 06
+*/
+
+const day = '06'
+const test: boolean = false
+// const testPart1: boolean = true
+// const testPart2: boolean = true
+
+const fileName = test ? `../testdata/${day}.txt` : `../data/${day}.txt`
+let inputLines = readFileSync(fileName).toString().split('\n')
+inputLines = inputLines.map(str => (str.trim()))
+
+const inputRegex = /(\d+)/g
+
+const timesStrings = inputLines[0].match(inputRegex)
+const distStrings = inputLines[1].match(inputRegex)
+if (distStrings === null || timesStrings === null) { console.log('Problems parsing'); process.exit(1) }
+timesStrings.push(timesStrings.join(''))
+distStrings.push(distStrings.join(''))
+const dist = distStrings.map(a => Number(a))
+const times = timesStrings.map(a => Number(a))
+
+let totalWinnings = 1
+let part2Winnings = 0
+times.forEach((playTime, idx) => {
+  let winnings = 0
+  const distToBeat = dist[idx]
+  for (let acc = 1; acc < playTime; acc++) {
+    const distance = acc * (playTime - acc)
+    if (distance > distToBeat) {
+      winnings += 1
+    }
+  }
+  if (idx < times.length - 1) {
+    totalWinnings *= winnings
+  } else {
+    part2Winnings = winnings
+  }
+})
+console.log('Result Part 1: ', totalWinnings)
+console.log('Result Part 2: ', part2Winnings)
